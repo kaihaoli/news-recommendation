e63da3be6ce877c4c4bc65c4b91c3ac90b55c405
Kaihao Li <kalli@cs.stonybrook.edu>
2018-04-18 19:51:29 -0400

    add-news-monitor

--- news_pipeline/news_monitor.py
+++ news_pipeline/news_monitor.py
@@ -1,1 +1,69 @@
 # -*- coding: utf-8 -*-
+
+import datetime
+import hashlib
+import os
+import redis
+import sys
+
+# dir
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+
+import news_api_client
+from cloudAMQP_client import CloudAMQPClient
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+NEWS_TIME_OUT_IN_SECONDS = 3600 * 24
+SLEEP_TIME_IN_SECOUNDS = 30
+
+# Clound AMQP
+SCRAPE_NEWS_TASK_QUEUE_URL = "amqp://bddugymf:6Vf5lxc4tfV8HHP1MSPJO3yzhl_iGlVR@eagle.rmq.cloudamqp.com/bddugymf"
+SCRAPE_NEWS_TASK_QUEUE_NAME = "tap-news-scrape-news-task-queue"
+
+NEWS_SOURCES = [
+    'bbc-news',
+    'bbc-sport',
+    'bloomberg',
+    'cnn',
+    'entertainment-weekly',
+    'espn',
+    'ign',
+    'techcrunch',
+    'the-new-york-times',
+    'the-wall-street-journal',
+    'the-washington-post'
+]
+
+# Redis - Remove Duplicate
+redis_client = redis.StrictRedis(REDIS_HOST, REDIS_PORT)
+
+# Could AMQP
+cloudAMQP_client = CloudAMQPClient(SCRAPE_NEWS_TASK_QUEUE_URL, SCRAPE_NEWS_TASK_QUEUE_NAME)
+
+while True:
+    # News List
+    news_list = news_api_client.getNewsFromSource(NEWS_SOURCES)
+    num_of_new_news = 0
+
+    # Traverse Each News
+    for news in news_list:
+        # Remove news has been in Redis
+        news_digest = hashlib.md5(news['title'].encode('utf-8')).digest().encode('base64')
+
+        if redis_client.get(news_digest) is None:
+            num_of_new_news = num_of_new_news + 1
+            news['digest'] = news_digest
+
+            if news['publishedAt'] is None:
+                # format: YYYY-MM-DDTHH:MM:SS in UTC
+                news['publishedAt'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+            redis_client.set(news_digest, news)
+            redis_client.expire(news_digest, NEWS_TIME_OUT_IN_SECONDS)
+
+            cloudAMQP_client.sendMessage(news)
+
+    print "Fetched %d new news." % num_of_new_news
+    cloudAMQP_client.sleep(SLEEP_TIME_IN_SECOUNDS)
